94ebc3fe43a93f2b6f573225bc9ade1e5240b739
Chris Birk <cmbirk@gmail.com>
2015-06-18 21:23:15 -0400

    use file hashes instead of timestamps for cachebusting

--- tasks/cachebust.js
+++ tasks/cachebust.js
@@ -3,8 +3,6 @@ var cachebust = require('gulp-cache-bust');
 
 gulp.task('cachebust', ['usemin'], function () {
   return gulp.src('./public/index.html')
-    .pipe(cachebust({
-      type: 'timestamp'
-    }))
+    .pipe(cachebust())
     .pipe(gulp.dest('public'));
 });
